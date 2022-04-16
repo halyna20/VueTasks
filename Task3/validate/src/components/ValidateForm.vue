01fac0e3a7415e10bc685eef4803d272a62aec82
halyna20 <halynka2000@gmail.com>
2022-04-16 16:51:50 +0300

    fix validation for radio

--- Task3/validate/src/components/ValidateForm.vue
+++ Task3/validate/src/components/ValidateForm.vue
@@ -1,119 +1,109 @@
 <template>
-    
     <div :style="{ background }" class="container">
-        <form @submit.prevent="onSubmit">
-                <label for="name">First name:</label>
-                <input type="text" id="name" name="name" v-model="name">
-                <span class="error"> {{ nameError }}</span>
-                <br/>
-                <label for="surname">Last name:</label>
-                <input type="text" id="surname" name="surname" v-model="surname"/>
-                <span class="error"> {{ surnameError }}</span>
-                <br/>
-                
-                <input type="radio" id="woman" value="woman" @change="check" v-model="picked" name="picked" >
-                <label for="woman">Woman</label>
+        <Form @submit="onSubmit"   :validation-schema="schema" v-slot= "{ meta }">
+            <label for="name">First name:</label>
+            <Field type="text" id="name" name="name"  />
+            <ErrorMessage name="name" class="error" />
+            <br/>
+            
+            <label for="surname">Last name:</label>
+            <Field type="text" id="surname" name="surname" />
+            <ErrorMessage name="surname" class="error" />
+            <br/>
 
-                <input type="radio" id="man" value="man" @change="check" v-model="picked" name="picked" >
-                <label for="man">Man</label>
-                <br/>
+            <Field name="gender" type="radio" value="woman" v-slot= "{ field }"> 
+                <input type="radio" id="woman" value="woman" @change="check" v-bind="field" v-model="gender" name="gender" >
+                <label for="woman">Woman</label> 
+            </Field>
+            <Field name="gender" type="radio" value="man" v-slot= "{ field }">
+                <input type="radio" id="man" value="man" @change="check" v-bind="field" v-model="gender" name="gender" >
+                <label for="man">Man</label> 
+            </Field>
+            <ErrorMessage name="gender" class="error"/>
+            <br/>
 
-                <label for="phone">Phone number: </label>
-                <input type="tel" id="phone" name="phone" v-model="phone">
-                <span class="error"> {{ phoneError }}</span>
-                <br/>
+            <label for="phone">Phone number: </label>
+            <Field type="tel" id="phone" name="phone" />
+            <ErrorMessage name="phone" class="error" /> 
+            <br/>
 
-                <label for="nickname">Nickname:</label>
-                <input type="text" id="nickname" name="nickname" v-model="nickname">
-                <span class="error"> {{ nicknameError }}</span>
-                <br/>
-                <button type="submit" :disabled="!meta.valid">Ok</button>
-        </form>
+            <label for="nickname">Nickname:</label>
+            <Field type="text" id="nickname" name="nickname" />
+            <ErrorMessage name="nickname" class="error" />
+            <br/>
+
+            <button type="submit" :disabled=!meta.valid>Ok</button>
+        </Form>
     </div>
 </template>
 
 <script>
-    import { useField, useForm, useFieldValue } from 'vee-validate';
-    
+    import { Form, Field, ErrorMessage } from 'vee-validate';
 
     export default {
         name: 'ValidateForm',
-        setup() {
-            const { meta } = useForm({
-                initialValues: {
-                    name: '',
-                    surname: '',
-                    phone: '',
-                    picked: '',
-                    nickname: ''
-                }
-            });
-
-            function onSubmit() {
-                alert('submited')
-            }
-
-            const isRequired = value => (value ? true : 'This field is required');
-
-            const name = useField('name', isRequired)
-
-            const surname = useField('surname', isRequired)
-            
-            const currentValue = useFieldValue('picked');
-            console.log(currentValue.value);
-
-            const phone = useField('phone', function(value) {
-                if(!value) return 'This field is required'
-
-                const regex = /^\+?38\s?(\(|\s)?\d{3}(\)|\s)?\s?\d{3}(\s|-)?\d{2}(\s|-)?\d{2}$/
-                if (!regex.test(String(value).toLowerCase())) {
-                    return 'Please enter a valid phone number'
-                }
-
-                return true;
-            })
-
-            const nickname = useField('nickname', function(value) {
-                if(!value) return 'This field is required'
-
-                const regex = /(?=.*[A-Z])(?=.*[_]).*\d{3}$/
-                if (!regex.test(String(value))) {
-                    return 'Please enter a valid nickname'
-                }
-
-                return true;
-            })
-
-            return {
-                meta,
-                onSubmit,
-                name: name.value,
-                nameError: name.errorMessage,
-                surname: surname.value,
-                surnameError: surname.errorMessage,
-                phone: phone.value,
-                phoneError: phone.errorMessage,
-                nickname: nickname.value,
-                nicknameError: nickname.errorMessage
-            }
-        },
+        components: { Form, Field, ErrorMessage },
         data() {
             return {
-                picked: '',
-                background: ''
+                gender: '',
+                background: '',
+                initialValues: {} ,
+                schema: {
+                    gender: (value) => {
+                        if (value) {
+                            return true;
+                        }
+                        return 'You must choose a gender';
+                    },
+                    name: (value) => {
+                        if (value) {
+                            return true;
+                        }
+                        
+                        return 'This field is required';
+                    },
+                    surname: (value) => {
+                        if (value) {
+                            return true;
+                        }
+                        
+                        return 'This field is required';
+                    },
+                    phone: (value) => {
+                        if(!value) return 'This field is required'
+
+                        const regex = /^\+?38\s?(\(|\s)?\d{3}(\)|\s)?\s?\d{3}(\s|-)?\d{2}(\s|-)?\d{2}$/
+                        if (!regex.test(String(value).toLowerCase())) {
+                            return 'Please enter a valid phone number'
+                        }
+
+                        return true;
+                    },
+                    nickname: (value) => {
+                        if(!value) return 'This field is required'
+
+                        const regex = /(?=.*[A-Z])(?=.*[_]).*\d{3}$/
+                        if (!regex.test(String(value))) {
+                            return 'Please enter a valid nickname'
+                        }
+
+                        return true;
+                    }
+                }
             }
         },
         methods: {
             check() {
-                if(this.picked === 'woman') {
+                if(this.gender === 'woman') {
                     this.background = 'pink'
-                } else if (this.picked === 'man') {
+                } else if (this.gender === 'man') {
                     this.background = 'blue'
                 }
             },
-
+            onSubmit() {
+                alert('submited')
+            }
         }
     }
 
-    
 </script>
